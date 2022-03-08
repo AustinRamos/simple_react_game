df1ca0864eff6d2dc2bdef00330ee4405eae4eed
Austin Ramos <austinramos@Austins-MacBook-Pro.local>
2022-03-08 13:38:56 -0800

    whatever

--- src/index.js
+++ src/index.js
@@ -2,7 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+//returns winner or null if draw.
+function CalculateWinner(squares){
+    //all possible winning lines.
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+      ];
 
+for(let i =0;i<lines.length;i++){
+const [a,b,c] = lines[i];
+if (squares[a] && squares[a]==squares[b] && squares[a]==squares[c]){
+    console.log("WINNER: " + squares[a]);
+return squares[a];
+}
+console.log("no winner :(");
+return null;
+}
+}
+
+
+
+//FUNCTION COMPONENT( NO STATE AND PURE)
 function Square(props){
     return(
         <button className="square" onClick={props.onClick}>
@@ -10,6 +37,9 @@ function Square(props){
     </button>
     );
 }
+
+
+
 
   class Board extends React.Component {
     
@@ -24,10 +54,16 @@ function Square(props){
     }
 
     handleClick(i){
-       const squares = this.state.squares.slice(); 
+        const squares = this.state.squares.slice(); 
+
+        if(squares[i] || CalculateWinner(this.state.squares)){
+        return;
+        }
         squares[i] = this.state.isXnext ? 'X' : 'O';
         this.setState({squares: squares,
                     isXnext: !this.state.isXnext});
+
+
     }
     
     renderSquare(i) {
@@ -38,11 +74,14 @@ function Square(props){
   
     render() {
 
-       // const curr_player = this.state.curr_player;
-      //const status = ('Next player: ' )
-      const status = ('Next player: ' + (this.state.isXnext ? 'X' : 'O'));
+        const winner = CalculateWinner(this.state.squares);
+        let status;
+        if(winner) {
+            status = 'Winner: ' + winner;
+        }else{
+            status = 'Next player: ' + (this.state.isXnext ? 'X' : 'O');
+        }
 
-      // this.setState({currplayer: 'O'})
       return (
         <div>
           <div className="status">{status}</div>
